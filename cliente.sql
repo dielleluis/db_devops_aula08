40efca4ff4f31af94209ffbac12e2950b9933dc9
Luis Dielle <luisdielle@Luiss-MacBook-Pro.local>
2020-04-24 22:03:49 -0300

    Adding sql file

---
+++ cliente.sql
@@ -0,0 +1,17 @@
+
+create table tbCliente(
+cpf varchar(11),
+nome varchar(100),
+end01 varchar(150),
+end02 varchar(150),
+bairro varchar(50),
+cidade varchar(50),
+estado varchar(50),
+cep varchar(8),
+dob date,
+idade smallint,
+sexo varchar(1),
+lim_cred float,
+vol_compras float,
+primeira_comp bit(1)
+);
